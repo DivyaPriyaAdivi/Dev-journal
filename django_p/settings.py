c21cf3f8fa62574aef545585fde056d1d1b6f515
DivyaPriyaAdivi <divyapriyaaaaa@gmail.com>
2025-09-13 12:28:40 +0530

    configure db

--- django_p/settings.py
+++ django_p/settings.py
@@ -84,13 +84,12 @@ WSGI_APPLICATION = 'django_p.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
-
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    "default": dj_database_url.config(
+        default=os.getenv("DATABASE_URL")  # Render provides DATABASE_URL in env
+    )
 }
+
 
 
 # Password validation
